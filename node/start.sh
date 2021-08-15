5444759b8b50b1ee1c510f16d3642ed1e63f29b4
阿七 <yiqiujia@hotmail.com>
2021-08-15 11:50:37 +0800

    Update start.sh
    -i node_modules

--- node/start.sh
+++ node/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #/usr/bin/nohup supervisor -w /node/ /node/server.js > /node/node.out 2>&1 & bash
-supervisor -w /node/ /node/server.js
+supervisor -w /node/ -i node_modules /node/server.js
